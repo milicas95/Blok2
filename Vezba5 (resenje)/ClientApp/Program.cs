8ad4bb88ca1f4b8499a16c87da6d5b6eeef57c6b
mivana <ivanadmarkanovic@gmail.com>
2017-11-22 14:21:23 +0100

    Projekat

---
+++ Vezba5 (resenje)/ClientApp/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using Manager;
+using System.Security.Principal;
+using DBparam;
+using System.IO;
+
+namespace ClientApp
+{
+	public class Program
+	{
+        static byte[] session_key = null;
+
+        static void Main(string[] args)
+		{
+            Console.ReadLine();
+			/// Define the expected service certificate. It is required to establish cmmunication using certificates.
+            string clientCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            IdentityReferenceCollection clGroups = WindowsIdentity.GetCurrent().Groups;
+            bool found = false;
+            string groupName = "";
+            foreach (IdentityReference group in clGroups)
+            {
+                SecurityIdentifier sid = (SecurityIdentifier)group.Translate(typeof(SecurityIdentifier));
+                var name = sid.Translate(typeof(NTAccount));
+                groupName = Formatter.ParseName(name.ToString());    /// return name of the Windows group				
+                if(groupName=="Admins" || groupName=="Writers" || groupName == "Readers")
+                {
+                    found = true;
+                    break;
+                }
+                
+            }
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+            if (!found)
+            {
+                Console.WriteLine("This client is not in any group.");
+                Console.ReadLine();
+            }
+            else
+            {
+                X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, "dkservice", "Servers");
+                EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/Receiver"),
+                                              new X509CertificateEndpointIdentity(srvCert));
+
+                using (SSLHandshakeClient proxyshake = new SSLHandshakeClient(binding, address))
+                {
+                    byte[] encrypted_session_key = null;
+                    X509Certificate2 serverCert = null;
+
+                    serverCert = proxyshake.RequestSession();
+                    if (serverCert == null)
+                    {
+                        Console.WriteLine("Error with servers public key!");
+                    }
+
+                    session_key = RSA_ASymm_Algorithm.GenerateSessionKey();
+
+                    encrypted_session_key = RSA_ASymm_Algorithm.RSAEncrypt(session_key, serverCert.PublicKey);
+                    if (encrypted_session_key == null)
+                    {
+                        Console.WriteLine("Error with encryption of session key!");
+                    }
+
+                    bool result = proxyshake.SendSessionKey(encrypted_session_key);
+                    if (result == false)
+                    {
+                        Console.WriteLine("Error with decryption of session key!");
+                    }
+                }
+
+                using (WCFClient proxy = new WCFClient(binding, address))
+                {
+                    int action;
+                    do
+                    {
+                        Console.WriteLine("================================================");
+                        Console.WriteLine("Wellcome {0} chose your action:", clientCertCN);
+                        Console.WriteLine("To create new data base press 1");
+                        Console.WriteLine("To delete exsisting data base press 2");
+                        Console.WriteLine("To make new parameter in data base press 3");
+                        Console.WriteLine("To modify existing parameter press 4");
+                        Console.WriteLine("To get average usage in city press 5");
+                        Console.WriteLine("To get average usage in region press 6");
+                        Console.WriteLine("To get highest spender in region press 7");
+                        Console.WriteLine("To exit app press 0");
+                        Console.WriteLine("================================================");
+                        action = Convert.ToInt32(Console.ReadLine());
+
+                        switch (action)
+                        {
+                            case 0:
+                                continue;
+                            case 3:
+                                DBParam dbp = new DBParam();
+                                Console.WriteLine("Enter ID: ");
+                                int id;
+                                string sid = Console.ReadLine();
+                                while (!Int32.TryParse(sid, out id))
+                                {
+                                    //               Audit.AddFailed("User " + userName + " put wrong parameter for year.");     //ispis u Log fajl za unos losih parametara
+                                    Console.WriteLine("Error, wrong input for year");
+                                }
+                                //dodati proveru da li postoji id u bazi
+                                dbp.Id = id;
+                                Console.WriteLine("Enter region: ");
+                                dbp.Region = Console.ReadLine();
+                                Console.WriteLine("Enter city: ");
+                                dbp.City = Console.ReadLine();
+                                Console.WriteLine("Enter year: ");
+                                int year;
+                                string syear = Console.ReadLine();
+                                while (!Int32.TryParse(syear, out year))
+                                {
+                                    //               Audit.AddFailed("User " + userName + " put wrong parameter for year.");     //ispis u Log fajl za unos losih parametara
+                                    Console.WriteLine("Error, wrong input for year");
+                                }
+                                dbp.Year = year;
+                                Console.WriteLine("Enter month: ");
+                                dbp.Month = Console.ReadLine();
+                                Console.WriteLine("Enter usage: ");
+                                int eUsage;
+                                string seUsage = Console.ReadLine();
+                                while (!Int32.TryParse(seUsage, out eUsage))
+                                {
+                                    //               Audit.AddFailed("User " + userName + " put wrong parameter for usage.");        //ispis u Log fajl za unos losih parametara
+                                    Console.WriteLine("Error, wrong input for usage");
+                                }
+                                dbp.ElEnergySpent = eUsage;
+                                if (proxy.Add(clientCertCN,dbp))
+                                {
+                                    Console.WriteLine("Added to database!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error! Information can't be added to database!");
+                                }
+                                continue;
+                            case 4:
+                                DBParam dbp1 = new DBParam();
+                                string path = "C://Users//Administrator.DOMAINADMINS0//Desktop//DataKeep//Vezba5 (resenje)//ServiceApp//bin//Debug//DataBase.txt";
+                                Console.WriteLine("Current database: ");
+                                using (StreamReader sr = new StreamReader(path))
+                                {
+                                    String line;
+                                    while ((line = sr.ReadLine()) != null)
+                                    {
+                                        Console.WriteLine(line);
+                                    }
+                                }
+                                Console.WriteLine("Enter ID of information you want to change: ");
+                                int id1;
+                                string sid1 = Console.ReadLine();
+                                while (!Int32.TryParse(sid1, out id1))
+                                {
+                                    //               Audit.AddFailed("User " + userName + " put wrong parameter for year.");     //ispis u Log fajl za unos losih parametara
+                                    Console.WriteLine("Error, wrong input for year");
+                                }
+                                //dodati proveru da li postoji id u bazi
+                                dbp1.Id = id1;
+
+
+
+                                if (proxy.Edit(clientCertCN,dbp1))
+                                {
+                                    Console.WriteLine("Database edited!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error! Information can't be edited!");
+                                }
+                                continue;
+                            case 5:
+                                Console.WriteLine("What is the city?");
+                                string city = Console.ReadLine();
+                                int averageCity = proxy.AverageUsageInCity(city, clientCertCN);
+                                if (averageCity == -1)
+                                    Console.WriteLine("You don't have promission to use this method");
+                                else
+                                    Console.WriteLine("Average usage in {0} is {1}", city, averageCity);
+                                continue;
+                            case 6:
+                                Console.WriteLine("What is the region?");
+                                string region = Console.ReadLine();
+                                int averageRegion = proxy.AverageUsageInRegion(region, clientCertCN);
+                                if (averageRegion == -1)
+                                    Console.WriteLine("You don't have promission to use this method");
+                                else
+                                    Console.WriteLine("Average usage in {0} is {1}", region, averageRegion);
+                                continue;
+                            case 7:
+                                Console.WriteLine("What is the region?");
+                                string reg = Console.ReadLine();
+                                string highestSpender = proxy.HighestSpenderInRegion(reg, clientCertCN);
+                                if (highestSpender == "You can't use this option")
+                                    Console.WriteLine("You don't have promission to use this method");
+                                else
+                                    Console.WriteLine("Highest spender in {0} is {1}", reg, highestSpender);
+                                continue;
+                            default:
+                                continue;
+                        }
+                    } while (action != 0);
+
+                }
+            }
+		}
+	}
+}
---
+++ Vezba5 (resenje)/ServiceApp/WCFService.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contracts;
+using Manager;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.IO;
+using DBparam;
+using System.ServiceModel;
+
+namespace ServiceApp
+{
+    
+    public class WCFService : IDatabaseManagement,ISSLHandshake
+    {
+
+        # region SSL Handshake
+        static byte[] session_key;
+
+        public X509Certificate2 RequestSession()
+        {
+            X509Certificate2 serverCertificate;
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            serverCertificate = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, srvCertCN, "Servers");
+            //serverCertificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN, "Servers");
+
+            return serverCertificate;
+        }
+
+        public bool SendSessionKey(byte[] encrypted_session_key)
+        {
+            //byte[] sessionkey = null;
+
+            X509Certificate2 serverCertificate;
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            serverCertificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN, "Servers");
+
+            session_key = RSA_ASymm_Algorithm.RSADecrypt(encrypted_session_key, serverCertificate);
+
+            return true;
+
+        }
+
+        public byte[] GetSessionKey()
+        {
+            return session_key;
+        }
+
+        #endregion
+
+        // autorizacija ide samo kod pravljenja i brisanja datoteka !!!!!!
+
+        public int AverageUsageInCity(string city, string userName)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Readers");
+            //         Audit.AuthorizationSuccess(userName, OperationContext.Current.IncomingMessageHeaders.Action);       //ispis da je autentifikovan korisnik
+            DBParam dbp = new DBParam();
+
+            string path = "DataBase.txt";
+            int totalUsage = 0;
+
+            if (cert != null)
+            {
+  //              Audit.CertificateSuccess();     //ispis u Log fajl da je ok certifikat
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    List<DBParam> wantedCity = new List<DBParam>();
+
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        string[] separeted = lines[i].Split('/');
+                        dbp.Id = Int32.Parse(separeted[0]);
+                        dbp.Region = separeted[1];
+                        dbp.City = separeted[2];
+                        dbp.Year = Int32.Parse(separeted[3]);
+                        dbp.Month = separeted[4];
+                        dbp.ElEnergySpent = Int32.Parse(separeted[5]);
+
+                        if (dbp.City == city)
+                        {
+                            wantedCity.Add(dbp);
+                        }
+                    }
+
+                    for (int i = 0; i < wantedCity.Count; i++)
+                    {
+                        totalUsage += wantedCity[i].ElEnergySpent;
+                    }
+                    totalUsage = totalUsage / wantedCity.Count;
+                }
+                catch (Exception e)
+                {
+                    Audit.ReadFailed(path, "Exception was thrown");     //ispis u Log fajlu da korisnik nema pravo pisanja
+                    Console.WriteLine("Exception was thrown:");
+                    Console.WriteLine(e.Message);
+                }
+
+                return totalUsage;
+            }
+            else
+            {
+         //       Audit.CertificateFailed();
+                Console.WriteLine("Certificate is invalid");
+                return -1;
+            }
+        }
+
+        public int AverageUsageInRegion(string region, string userName)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Readers");
+            //         Audit.AuthorizationSuccess(userName, OperationContext.Current.IncomingMessageHeaders.Action);       //ispis da je autentifikovan korisnik
+            DBParam dbp = new DBParam();
+
+            string path = "DataBase.txt";
+            int totalUsage = 0;
+
+            if (cert != null)
+            {
+//                Audit.CertificateSuccess();     //ispis u Log fajl da je ok certifikat
+
+                try
+                {
+   //                 Audit.ReadSuccess(path);        //ispis u Log fajl da je uspesno procitana (otvorena) datoteka
+
+                    string[] lines = File.ReadAllLines(path);
+                    List<DBParam> wantedReg = new List<DBParam>();
+
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        string[] separeted = lines[i].Split('/');
+                        dbp.Id = Int32.Parse(separeted[0]);
+                        dbp.Region = separeted[1];
+                        dbp.City = separeted[2];
+                        dbp.Year = Int32.Parse(separeted[3]);
+                        dbp.Month = separeted[4];
+                        dbp.ElEnergySpent = Int32.Parse(separeted[5]);
+
+                        if (dbp.Region == region)
+                        {
+                            wantedReg.Add(dbp);
+                        }
+                    }
+
+                    for (int i = 0; i < wantedReg.Count; i++)
+                    {
+                        totalUsage += wantedReg[i].ElEnergySpent;
+                    }
+
+                    totalUsage = totalUsage / wantedReg.Count;
+                }
+                catch (Exception e)
+                {
+  //                  Audit.ReadFailed(path,"Exception was thrown");     //ispis u Log fajlu da korisnik nema pravo pisanja
+                    Console.WriteLine("Exception was thrown:");
+                    Console.WriteLine(e.Message);
+                }
+
+                return totalUsage;
+            }
+            else
+            {
+  //              Audit.CertificateFailed();
+                Console.WriteLine("Certificate is invalid");
+                return -1;
+            }
+        }
+
+        public string HighestSpenderInRegion(string region, string userName)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Readers");
+            //           Audit.AuthorizationSuccess(userName, OperationContext.Current.IncomingMessageHeaders.Action);       //ispis da je autentifikovan korisnik
+            DBParam dbp = new DBParam();
+
+            string path = "DataBase.txt";
+            string hs="";
+
+            if (cert != null)
+            {
+  //              Audit.CertificateSuccess();     //ispis u Log fajl da je ok certifikat
+
+                try
+                {
+//                    Audit.ReadSuccess(path);        //ispis u Log fajl da je uspesno procitana (otvorena) datoteka
+
+                    string[] lines = File.ReadAllLines(path);
+
+                    List<DBParam> wantedReg = new List<DBParam>();
+
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        string[] separeted = lines[i].Split('/');
+                        dbp.Id = Int32.Parse(separeted[0]);
+                        dbp.Region = separeted[1];
+                        dbp.City = separeted[2];
+                        dbp.Year = Int32.Parse(separeted[3]);
+                        dbp.Month = separeted[4];
+                        dbp.ElEnergySpent = Int32.Parse(separeted[5]);
+
+                        if (dbp.Region == region)
+                        {
+                            wantedReg.Add(dbp);
+                        }
+                    }
+
+                    Dictionary<string, int> citiesInRegion = new Dictionary<string, int>();
+                    foreach (DBParam p in wantedReg)
+                    {
+                        if (citiesInRegion.ContainsKey(p.City))
+                        {
+                            citiesInRegion[p.City] += p.ElEnergySpent;
+                        }
+                        else
+                            citiesInRegion.Add(p.City, p.ElEnergySpent);
+                    }
+                    List<KeyValuePair<string, int>> lista = citiesInRegion.ToList();
+                    hs = lista[0].Key;
+                    int hsVal = lista[0].Value;
+                    for (int i = 1; i < citiesInRegion.Count; i++)
+                    {
+                        if (lista[i].Value > hsVal)
+                        {
+                            hsVal = lista[i].Value;
+                            hs = lista[i].Key;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+  //                  Audit.ReadFailed(path, "Exception was thrown");     //ispis u Log fajlu da korisnik nema pravo pisanja
+                    Console.WriteLine("Exception was thrown:");
+                    Console.WriteLine(e.Message);
+                }
+
+                return hs;
+            }
+            else
+            {
+              //  Audit.CertificateFailed();
+                Console.WriteLine("Certificate is invalid");
+                return "You can't use this option";
+            }
+        }
+
+        public bool Add(string userName, DBParam dbp)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Writers");
+            //    Audit.AuthorizationSuccess(userName, OperationContext.Current.IncomingMessageHeaders.Action);       //ispis da je autentifikovan korisnik
+            string path = "DataBase.txt";
+
+            if (cert != null)
+            {
+                //   Audit.CertificateSuccess();     //ispis u Log fajl da je ok certifikat
+                //      Audit.ReadSuccess(database);        //ispis u Log fajl da je uspesno procitana (otvorena) datoteka
+
+                try
+                {
+
+                    using (StreamWriter sw = new StreamWriter(path, true))
+                    {
+                        sw.WriteLine(dbp.Id + "/" + dbp.Region + "/" + dbp.City + "/" + dbp.Year + "/" + dbp.Month + "/" + dbp.ElEnergySpent);
+                    }
+
+                    dbp.IDs.Add(dbp.Id);
+                    //         Audit.AddSuccess();         //ispis u Log fajlu da je dodat podatak u datoteku
+                }
+                catch (Exception e)
+                {
+                    //               Audit.AddFailed("Exception was thrown");     //ispis u Log fajlu da korisnik nema pravo pisanja
+                    Console.WriteLine("Exception was thrown:");
+                    Console.WriteLine(e.Message);
+                }
+
+                return true;
+            }
+            else
+            {
+                //           Audit.CertificateFailed();
+                Console.WriteLine("Certificate is invalid");
+                return false;
+            }
+        }
+
+        public bool Edit(string userName, DBParam dbp)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Writers");
+            //       Audit.AuthorizationSuccess(userName, OperationContext.Current.IncomingMessageHeaders.Action);       //ispis da je autentifikovan korisnik
+            string path = "DataBase.txt";
+
+            if (cert != null)
+            {
+                //            Audit.CertificateSuccess();     //ispis u Log fajl da je ok certifikat
+
+                try
+                {
+                //                    Audit.ReadSuccess(database);        //ispis u Log fajl da je uspesno procitana (otvorena) datoteka
+                    
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        sw.WriteLine(dbp.Id + "/" + dbp.Region + "/" + dbp.City + "/" + dbp.Year + "/" + dbp.Month + "/" + dbp.ElEnergySpent);
+                    }
+
+                    //       Audit.UpdateSuccess();
+                 }
+                 catch (Exception e)
+                 {
+                 //              Audit.UpdateFailed("Exception was thrown");     //ispis u Log fajlu da korisnik nema pravo pisanja
+                    Console.WriteLine("Exception was thrown: ");
+                    Console.WriteLine(e.Message);
+                 }
+
+                 return true;
+             }
+             else
+             {
+                //            Audit.CertificateFailed();
+                Console.WriteLine("Certificate is invalid");
+                return false;
+            }
+        }
+
+        public bool CreateDatabase(string userName)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Admins");
+
+            string path = "DataBase.txt";
+
+            if (cert != null)
+            {
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    try
+                    {
+                        File.CreateText(path);
+                        Console.WriteLine("Successfuly created new database!");
+                        return true;
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Database already exists");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Certificate is invalid");
+                return false;
+            }
+        }
+
+        public bool DeleteDatabase(string userName)
+        {
+            X509Certificate2 cert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, userName, "Admins");
+
+            string path = "DataBase.txt";
+
+            if (cert != null)
+            {
+                if (File.Exists(path))
+                {
+                    // Delete a file 
+                    try
+                    {
+                        File.Delete(path);
+                        Console.WriteLine("Successfuly deleted database!");
+                        return true;
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Database already exists");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Certificate is invalid");
+                return false;
+            }
+        }
+
+    }
+}
