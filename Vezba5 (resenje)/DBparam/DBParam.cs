63d1e0c6beb0bfb48bb15e7c601908344788a820
mivana <ivanadmarkanovic@gmail.com>
2017-11-22 14:24:03 +0100

    Revert "Revert "Projekat""
    This reverts commit fc12e7422859b1a25459a8397e8d0befca1978cc.

---
+++ Vezba5 (resenje)/DBparam/DBParam.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBparam
+{
+    public class DBParam
+    {
+        private int id;
+        private string region;
+        private string city;
+        private int year;
+        private string month;
+        private int elEnergySpent;
+        private List<int> ids;          //list svih IDjeva u bazi
+
+        public DBParam() { }
+
+        public List<int> IDs { get; set; }
+
+        public int Id { get; set; }
+
+        public string Region { get; set; }
+
+        public string City
+        {
+            get;
+            set;
+        }
+        public int Year
+        {
+            get;
+            set;
+        }
+        public string Month
+        {
+            get;
+            set;
+        }
+        public int ElEnergySpent
+        {
+            get;
+            set;
+        }
+    }
+}
